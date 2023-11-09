bfb16a79a1de9bd50b2a654dc46023409ba67f3c
JakobYdeNaerum <126673163+JakobYdeNaerum@users.noreply.github.com>
2023-11-09 15:15:35 +0100

    123

--- app.js
+++ app.js
@@ -20,20 +20,3 @@ document.addEventListener('DOMContentLoaded', function () {
     var logo = document.getElementById('logostart');
     logo.classList.add('logotransform');
 });
-
-//Knap til video
-document.getElementById('fremvideo').addEventListener('click', function() {
-    var video = document.getElementById('videoPlayer');
-    video.style.visibility = 'visible';  // Gør videoen synlig
-    video.play();  // Starter afspilning af videoen
-    video.setAttribute('controls', true); // Viser kontrolknapper (play, pause, osv.)
-    this.style.display = 'none';  // Skjuler knappen
-
-    // Flytter visningen til videoen
-    video.scrollIntoView({ behavior: 'smooth', block: 'start' });
-});
-
-
-
-
-
