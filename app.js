8bdb97995d6f8b683b27414981e505359c53589d
MajedDadou <91125807+MajedDadou@users.noreply.github.com>
2023-11-09 09:30:53 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// window.addEventListener('scroll', function() {
+//     let offset = window.scrollY;
+//     document.getElementById('section1').style.backgroundPositionY = offset * 0.5 + 'px';
+//     document.getElementById('section2').style.backgroundPositionY = offset * 0.5 + 'px';
+// });
+
+window.addEventListener('scroll', function () {
+    let offset = window.scrollY;
+    for (let i = 1; i <= 10; i++) {
+        document.getElementById('section' + i).style.backgroundPositionY = offset * 0.01 + 'px';
+        // document.getElementById('section2').style.backgroundPositionY = offset * 0.1 + 'px';
+        // document.getElementById('section3').style.backgroundPositionY = offset * -0.1 + 'px';
+
+
+    }
+});
+
+
