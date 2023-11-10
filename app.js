cb63b10eecebc1cc403d796f809650779600d630
matildemaribo <126452785+matildemaribo@users.noreply.github.com>
2023-11-10 17:44:25 +0100

    ændring af space unden indhold
    orden i kode

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ window.addEventListener('scroll', function () {
         document.getElementById('section' + i).style.backgroundPositionY = offset * 0.0 + 'px';
         // document.getElementById('section2').style.backgroundPositionY = offset * 0.1 + 'px';
         // document.getElementById('section3').style.backgroundPositionY = offset * -0.1 + 'px';
-
-
     }
 });
 
@@ -20,18 +18,4 @@ document.addEventListener('DOMContentLoaded', function () {
     var logo = document.getElementById('logostart');
     logo.classList.add('logotransform');
 });
-
-//Knap for at vise video
-    // Knap til video
-    document.getElementById('fremvideo').addEventListener('click', function() {
-        var video = document.getElementById('videoPlayer');
-        
-        video.style.visibility = 'visible';  // Gør videoen synlig
-        video.play();  // Starter afspilning af videoen
-        video.setAttribute('controls', true); // Viser kontrolknapper (play, pause, osv.)
-        this.style.display = 'none';  // Skjuler knappen
-
-        // Flytter visningen til videoen
-        video.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    });
 
