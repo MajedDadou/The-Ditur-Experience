3f546aca67827131261228a48ba107f466fefffb
MajedDadou <91125807+MajedDadou@users.noreply.github.com>
2023-11-13 12:38:42 +0100

    update

--- app.js
+++ app.js
@@ -4,16 +4,21 @@
 //     document.getElementById('section2').style.backgroundPositionY = offset * 0.5 + 'px';
 // });
 
-window.addEventListener('scroll', function () {
-    let offset = window.scrollY;
-    for (let i = 1; i <= 10; i++) {
-        document.getElementById('section' + i).style.backgroundPositionY = offset * 0.0 + 'px';
-        // document.getElementById('section2').style.backgroundPositionY = offset * 0.1 + 'px';
-        // document.getElementById('section3').style.backgroundPositionY = offset * -0.1 + 'px';
-    }
-});
+// window.addEventListener('scroll', function () {
+//     let offset = window.scrollY;
+//     for (let i = 1; i <= 10; i++) {
+//         document.getElementById('section' + i).style.backgroundPositionY = offset * 0.0 + 'px';
+//         // document.getElementById('section2').style.backgroundPositionY = offset * 0.1 + 'px';
+//         // document.getElementById('section3').style.backgroundPositionY = offset * -0.1 + 'px';
+//     }
+// });
 
 //Logo transition
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     var logo = document.getElementById('logostart');
     logo.classList.add('logotransform');
@@ -30,14 +35,3 @@ function toggleMenu() {
 }
 
 
-window.addEventListener('scroll', function () {
-    var scrollPosition = window.scrollY;
-    var upperNav = document.getElementById('upperNav');
-
-    // Adjust the scroll position value based on when you want the upperNav to be visible
-    if (scrollPosition >= 500) {
-        upperNav.style.display = 'block'; // Make the upperNav visible
-    } else {
-        upperNav.style.display = 'none'; // Hide the upperNav
-    }
-});
