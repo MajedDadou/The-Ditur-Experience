e4da100432c4d75e949bc8d1b786f6574c6b0e2b
MajedDadou <91125807+MajedDadou@users.noreply.github.com>
2023-11-13 14:54:57 +0100

    update

--- app.js
+++ app.js
@@ -35,82 +35,12 @@ function toggleMenu() {
 }
 
 
-// document.addEventListener('scroll', function () {
-//     var nav = document.getElementById('upper-nav');
 
-//     // Check the visibility of each section
-//     for (var i = 1; i <= 8; i++) {
-//         var sectionId = 'section' + i;
-//         var section = document.getElementById(sectionId);
-
-//         if (section) {
-//             var rect = section.getBoundingClientRect();
-
-//             if (rect.top <= window.innerHeight / 4 && rect.bottom >= window.innerHeight / 4) {
-//                 // Section is in the viewport
-//                 nav.style.display = 'block';
-//             }else if (rect.bottom >= window.innerHeight) {
-//                     // Section is in the viewport
-//                     nav.style.display = 'none';
-//                 }
-//             }
-//             // Add "bolder" class for sections 2 to 9
-//             if (i >= 2 && i <= 9) {
-//                 nav.classList.add('bolder');
-//             } else {
-//                 nav.classList.remove('bolder');
-//             }
-
-//             return; // Exit the loop if a visible section is found
-//         }
-//     }
-
-
-//     // If no section is in the viewport, hide the navigation
-//     // nav.style.display = 'none';
-// );
-
-
-
-
-
-
-// document.addEventListener('scroll', function () {
-//     var nav = document.getElementById('upper-nav');
-
-//     // Check the visibility of each section
-//     for (var i = 1; i <= 8; i++) {
-//         var sectionId = 'section' + i;
-//         var section = document.getElementById(sectionId);
-
-//         if (section) {
-//             var rect = section.getBoundingClientRect();
-
-//             if (rect.top <= window.innerHeight / 4 && rect.bottom >= window.innerHeight / 1) {
-//                 // Section is in the viewport
-//                 nav.style.display = 'block';
-
-//                 // Add "bolder" class for sections 2 to 9
-//                 if (i >= 2 && i <= 9) {
-//                     nav.classList.add('bolder');
-//                 } else {
-//                     nav.classList.remove('bolder');
-//                 }
-
-//                 return; // Exit the loop if a visible section is found
-//             }
-//         }
-//     }
-
-//     // If no section is in the viewport, hide the navigation
-//     nav.style.display = 'none';
-// });
 
 
 
 document.addEventListener('scroll', function () {
     var nav = document.getElementById('upper-nav');
-    var visibleSection = null;
 
     // Check the visibility of each section
     for (var i = 1; i <= 8; i++) {
@@ -120,21 +50,16 @@ document.addEventListener('scroll', function () {
         if (section) {
             var rect = section.getBoundingClientRect();
 
-            if (rect.top <= window.innerHeight  && rect.bottom > window.innerHeight ) {
-                // Section is fully in the viewport
-                visibleSection = section;
-                break; // Exit the loop if a visible section is found
+            if (rect.top < window.innerHeight  && rect.bottom >= window.innerHeight / 1) {
+                // Section is in the viewport
+                nav.style.display = 'block';
+                return; // Exit the loop if a visible section is found
             }
-
         }
     }
 
-    // Show or hide the navigation based on section visibility
-    if (visibleSection) {
-        nav.style.display = 'block';
-        nav.classList.add('bolder');
-    } else {
-        nav.style.display = 'none';
-        nav.classList.remove('bolder');
-    }
+    nav.style.display = 'none';
 });
+
+
+
